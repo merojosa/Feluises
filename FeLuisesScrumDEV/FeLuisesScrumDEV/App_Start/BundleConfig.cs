e6a631ccba047b47838894e46932a9006955e3c6
LuisC <luisda7cr@gmail.com>
2019-09-27 01:24:26 -0600

    Ya Funca el Dropdown

--- FeLuisesScrumDEV/FeLuisesScrumDEV/App_Start/BundleConfig.cs
+++ FeLuisesScrumDEV/FeLuisesScrumDEV/App_Start/BundleConfig.cs
@@ -25,6 +25,10 @@ namespace FeLuisesScrumDEV
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                        "~/Scripts/materializeScripts.js"));
+
         }
     }
 }
---
+++ FeLuisesScrumDEV/FeLuisesScrumDEV/Scripts/materializeScripts.js
@@ -0,0 +1,21 @@
+﻿/*
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.dropdown-trigger'); //Se seleccionn todos los elementos en el DOM que contengan la clase dropdown-trigger
+    var instances = M.Dropdown.init(elems); //inicializa los elementos
+});*/
+
+
+(function ($) {
+    $(function () {
+
+        $('.dropdown-trigger').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            hover: true, // Activate on hover
+            belowOrigin: true, // Displays dropdown below the button
+            alignment: 'right' // Displays dropdown with edge aligned to the left of button
+        }
+        );
+
+    }); // End Document Ready
+})(jQuery); // End of jQuery name space
