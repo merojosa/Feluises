940c8336ed1cfca88d0af6b3bd53557708453cc8
Felipe De Jesus Solis Jimenez <b66993@ecci.ucr.ac.cr>
2019-10-05 17:51:08 -0600

    Listoooo

--- FeLuisesScrumDEV/FeLuisesScrumDEV/Controllers/loginController.cs
+++ FeLuisesScrumDEV/FeLuisesScrumDEV/Controllers/loginController.cs
@@ -32,9 +32,7 @@ namespace FeLuisesScrumDEV.Controllers
                 {
                     //Session["userID"] = userDetails.userID;
                     //Session["userName"] = userDetails.UserName;
-
                     Boolean isEmployee = false;
-                    // String myController = "";
 
                     //Sea cliente o empleado, hay que sacar su rol e id
                     var Employee = db.Employee.Find(userDetails.userName);
@@ -45,96 +43,20 @@ namespace FeLuisesScrumDEV.Controllers
                     }
                     else
                     {
-                        //Es empleado, entonces tenemos que loguearnos como empleado
-                        //Hay que sacar su rol e id
                         isEmployee = true;
                     }
 
                     if (isEmployee)
                     {
-
-                        //ViewBag.role = "Employees";
-                        //return View("Index", "Employee");
-                        //myController = "Employees";
-                        //ViewData["myControlle"] = "Employees";
-                        //return RedirectToAction("Index", "Employees"); //con id y rol de empleado
                         return RedirectToAction("Index", "Employees");
                     }
                     else
                     {
-                        //ViewBag.role = "Client";
-                        //return View(); //con id y rol de cliente
-                        //ViewData["myController"] = "Clients";
-                        // myController = "Clients";
-                        //return RedirectToAction("Index", "Clients");
                         return RedirectToAction("Index", "Clients");
                     }
-                    //return RedirectToAction("Index", myController);
 
-
-
-                    //return RedirectToAction("Index", "Home");
                 }
             }
         }
-
-        //    [HttpPost]
-        //    [ValidateAntiForgeryToken]
-        //    public ActionResult action([Bind(Include = "userName,password")] Credentials credentials)
-        //    {
-        //        Boolean registered = false;
-        //        //Verifica usuario en tabla de credenciales
-        //        var userCredentials = db.Credentials.Find(credentials.userName);
-        //        if (userCredentials != null)
-        //        {
-        //            //Verifacamos el password
-        //            if (userCredentials.password == credentials.password)
-        //            {
-        //                registered = true;
-        //            }
-
-        //            //Si registrado, busca en empleados o en cliente
-        //            if (registered)
-        //            {
-        //                Boolean isEmployee = false;
-        //               // String myController = "";
-
-        //                //Sea cliente o empleado, hay que sacar su rol e id
-        //                var Employee = db.Employee.Find(credentials.userName);
-        //                if (Employee == null)
-        //                {
-        //                    //Tiene que ser cliente
-        //                    var Client = db.Client.Find(credentials.userName);
-        //                } else {
-        //                    //Es empleado, entonces tenemos que loguearnos como empleado
-        //                    //Hay que sacar su rol e id
-        //                    isEmployee = true;
-        //                }
-
-        //                if (isEmployee) {
-
-        //                    //ViewBag.role = "Employees";
-        //                    //return View("Index", "Employee");
-        //                    //myController = "Employees";
-        //                    ViewData["myControlle"] = "Employees";
-        //                    //return RedirectToAction("Index", "Employees"); //con id y rol de empleado
-        //                    return RedirectToAction("Index, Employes");
-        //                } else {
-        //                    //ViewBag.role = "Client";
-        //                    //return View(); //con id y rol de cliente
-        //                    ViewData["myController"] = "Clients";
-        //                    // myController = "Clients";
-        //                    //return RedirectToAction("Index", "Clients");
-        //                    return RedirectToAction("Index", "Clients");
-        //                }
-        //                //return RedirectToAction("Index", myController);
-        //            }
-        //        }
-        //        //Este view debe retornar a la misma pantalla de login pero con un mensaje de error
-        //        return View("Login");   
-        //    }
-
-
-        //}
     }
 }
