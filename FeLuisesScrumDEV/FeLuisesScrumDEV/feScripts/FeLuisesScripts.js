69a046617b20a2a44511d108b0def5e65ed526dc
LuisC CITIC <luisda7cr@gmail.com>
2019-10-09 17:47:57 -0600

    Provincia-Canton-Distrito listo

--- FeLuisesScrumDEV/FeLuisesScrumDEV/feScripts/FeLuisesScripts.js
+++ FeLuisesScrumDEV/FeLuisesScrumDEV/feScripts/FeLuisesScripts.js
@@ -1,1 +1,57 @@
 ﻿
+
+function getProvincia(){ 
+    $.ajax({
+        dataType: "json",
+        url: "https://ubicaciones.paginasweb.cr/provincias.json",
+        data: {},
+        success: function (data) {
+            var html = "";
+            for (key in data) {
+              html += "<option value='" + key + "'>" + data[key] + "</option>";
+            }
+            $("#province").empty();
+            $("#province").append(html);
+        }
+    });
+}
+
+function getCanton() {
+
+    var id_province = $("#province").val();
+
+    $.ajax({
+        dataType: "json",
+        url: "https://ubicaciones.paginasweb.cr/provincia/"+id_province+"/cantones.json",
+        data: {},
+        success: function (data) {
+            var html = "";
+            for (key in data) {
+                html += "<option value='" + key + "'>" + data[key] + "</option>";
+            }
+            $("#canton").empty();
+            $("#canton").append(html);
+        }
+    });
+}
+
+function getDistrict() {
+
+    var id_province = $("#province").val();
+    var id_canton = $("#canton").val();
+
+    $.ajax({
+        dataType: "json",
+        url: "https://ubicaciones.paginasweb.cr/provincia/" + id_province + "/canton/" + id_canton + "/distritos.json",
+        data: {},
+        success: function (data) {
+            var html = "";
+            for (key in data) {
+                html += "<option value='" + key + "'>" + data[key] + "</option>";
+            }
+            console.log(data);
+            $("#district").empty();
+            $("#district").append(html);
+        }
+    });
+}
