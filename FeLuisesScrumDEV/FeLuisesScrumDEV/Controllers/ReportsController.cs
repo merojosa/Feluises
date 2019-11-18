9dd9fd92d05c57ed8595657dfc3413deff7947bb
felipeSolisJimenez <felipesolisjimenez@hotmail.com>
2019-11-17 21:00:57 -0600

    controlador reportes

---
+++ FeLuisesScrumDEV/FeLuisesScrumDEV/Controllers/ReportsController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FeLuisesScrumDEV.Controllers
+{
+    public class ReportsController : Controller
+    {
+        //La pantalla en la que estan todos los accesos a los reportes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Ver todos los desarrolladores, disponibles y ocupados, en su proyecto
+        public ActionResult DeveloperState()
+        {
+            return View();
+        }
+
+        //Conocer los periodos de desocupación de los desarrolladores.
+        public ActionResult InactivityPeriod()
+        {
+            return View();
+        }
+
+        //Buscar desarrolladores de acuerdo al tipo de conocimiento y disponibilidad.
+        public ActionResult devKnowledgeDisponibility()
+        {
+            return View();
+        }
+
+        //Cantidad de requerimientos por desarrollador para un proyecto específico
+        public ActionResult numRequirmnetsPerDev()
+        {
+            return View();
+        }
+
+        //Comparar la duración estimada y real para requerimiento de un desarrollador.
+        public ActionResult estimatedRealTimeDev()
+        {
+            return View();
+        }
+
+        //Total de horas estimadas y reales requeridas por un proyecto.
+        public ActionResult totalHours()
+        {
+            return View();
+        }
+
+
+        //Historial de participación de un desarrollador en diferentes proyectos.
+        public ActionResult paticipationHistory()
+        {
+            return View();
+        }
+
+        //Ver el estado de avance de un requerimiento.
+        public ActionResult requirmentState()
+        {
+            return View();
+        }
+
+        //Comparar las duraciones reales y estimadas entre requerimientos de un mismo nivel de complejidad.
+        public ActionResult compareDuration()
+        {
+            return View();
+        }
+
+
+        //Cantidad de desarrolladores con conocimientos específicos y promedio de antigüedad laboral.
+        public ActionResult devsInKnownledgeAndAtiguedad()
+        {
+            return View();
+        }
+
+
+
+        //Ver el estado y responsables de un requerimiento.
+        public ActionResult stateResponsableRequirement()
+        {
+            return View();
+        }
+
+        //Ver el total de requerimientos de un proyecto.
+        public ActionResult totalRequirements()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+    }
+}
