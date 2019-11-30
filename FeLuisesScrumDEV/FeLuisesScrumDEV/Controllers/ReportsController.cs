aebc2843f82fc2f6d100fe43f7dfb3dbddcfbdcc
LuisC <luisda7cr@gmail.com>
2019-11-30 02:22:45 -0600

    Problema en la que la consulta no devuelve nada

--- FeLuisesScrumDEV/FeLuisesScrumDEV/Controllers/ReportsController.cs
+++ FeLuisesScrumDEV/FeLuisesScrumDEV/Controllers/ReportsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using FeLuisesScrumDEV.Models;
+using FeLuisesScrumDEV.viewModel;
 
 namespace FeLuisesScrumDEV.Controllers
 {
@@ -87,7 +88,53 @@ namespace FeLuisesScrumDEV.Controllers
         //Ver el estado y responsables de un requerimiento. Según un cliente
         public ActionResult stateResponsableRequirement()
         {
+            //Como se usa una PartialView, la consulta esá abajo
             return View();
+        }
+        //EFE: Realiza la consulta de: Ver el estado y responsables de un requerimiento. Según un cliente
+        //REQ: Id del cliente que se obtiene de la variable de session y el IdProject que se obtiene del dropdown
+        public PartialViewResult stateResponsableReqClient(string idClientPK, int? idProject)
+        {
+            var client = idClientPK;
+            var rClient = client.ToString();
+
+            var query =
+                from c in db.Client
+                join p in db.Project on c.idClientPK equals p.idClientFK
+                join m in db.Module on p.idProjectPK equals m.idProjectFKPK
+                join r in db.Requeriment on m.idModulePK equals r.idModuleFKPK
+                join e in db.Employee on r.idEmployeeFK equals e.idEmployeePK
+                where r.idProjectFKPK == p.idProjectPK
+                && c.idClientPK == idClientPK
+                && p.idProjectPK == idProject
+                select new
+                {
+                    Nombre = r.objective,
+                    Estado = r.status,
+                    Responable = e.employeeName
+                };
+            var query2 =
+                from c in db.Client
+                join p in db.Project on c.idClientPK equals p.idClientFK
+                join wi in db.WorksIn on p.idProjectPK equals wi.idProjectFKPK
+                join e in db.Employee on wi.idEmployeeFKPK equals e.idEmployeePK
+                join r in db.Requeriment on p.idProjectPK equals r.idProjectFKPK
+                where c.idClientPK == rClient
+                && p.idProjectPK == idProject
+                select new
+                {
+                    Nombre = r.objective,
+                    Estado = r.status,
+                    Responable = e.employeeName
+                };
+
+            var results = query.ToList().Select(r => new StateResponsableReqClient_Result_Mapped
+            {
+                Nombre = r.Nombre,
+                Estado = r.Estado,
+                Responsable = r.Responable
+            }).ToList();
+            return PartialView("stateResponsableReqClient", results);
         }
 
 
