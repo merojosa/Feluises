3d6c9b7be8c35368cffd468e127beb9afc2469eb
Diego-02 <luis.naranjomonge@ucr.ac.cr>
2019-12-01 03:06:02 -0600

    rehaciendo el sprint 3 kappa

--- FeLuisesScrumDEV/FeLuisesScrumDEV/Controllers/ReportsController.cs
+++ FeLuisesScrumDEV/FeLuisesScrumDEV/Controllers/ReportsController.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FeLuisesScrumDEV.Models;
+using FeLuisesScrumDEV.viewModel;
 
 namespace FeLuisesScrumDEV.Controllers
 {
     public class ReportsController : Controller
     {
+
+        private FeLuisesEntities db = new FeLuisesEntities();
+
         //La pantalla en la que estan todos los accesos a los reportes
         public ActionResult Index()
         {
@@ -64,16 +70,63 @@ namespace FeLuisesScrumDEV.Controllers
         }
 
         //Comparar las duraciones reales y estimadas entre requerimientos de un mismo nivel de complejidad.
-        public ActionResult compareDuration()
+        public ActionResult CompareReqStatsbyComplexity()
+        {
+            var query = from r in db.Requeriment
+                        where r.status == 3
+                        group r by r.complexity into rGroup
+                        select new
+                        {
+                            Dificultad = rGroup.Key,
+                            Total = rGroup.Count(),
+                            Minima_Diff = rGroup.Min(x => Math.Abs((int)x.estimatedDuration - (int)x.realDuration)),
+                            Max_Diff = rGroup.Max(x => Math.Abs((int)x.estimatedDuration - (int)x.realDuration)),
+                            Avg_Diff = rGroup.Average(x => Math.Abs((int)x.estimatedDuration - (int)x.realDuration)),
+                            Avg_Real = rGroup.Average(x => x.realDuration),
+                            Avg_Est = rGroup.Average(x => x.estimatedDuration)
+                        };
+            var results = query.ToList().Select(r => new GetReqStatsbyComplexity_Result_Mapped
+            {
+                Dificultad = r.Dificultad,
+                Total = r.Total,
+                Minima_Diff = r.Minima_Diff,
+                Max_Diff = r.Max_Diff,
+                Avg_Diff = (int)r.Avg_Diff,
+                Avg_Real = (int)r.Avg_Real,
+                Avg_Est = (int)r.Avg_Est
+            }).ToList();
+            return View(results);
+        }
+
+        public ActionResult DevsKnowledgeLaborSeniority()
         {
             return View();
         }
 
-
         //Cantidad de desarrolladores con conocimientos específicos y promedio de antigüedad laboral.
-        public ActionResult devsInKnownledgeAndAtiguedad()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DevsKnowledgeLaborSeniority([Bind(Include = "Knowledge")]string knowledge)
         {
-            return View();
+            var now = DateTime.Now;
+            var basequery = db.DeveloperKnowledge.Include(c => c.idEmployeeFKPK);
+            var query = from dk in basequery
+                        where dk.devKnowledgePK.ToLower().Contains(knowledge.ToLower())
+                        group dk by dk.devKnowledgePK into dkGroup
+                        orderby dkGroup.Key descending
+                        select new
+                        {
+                            Conocimiento = dkGroup.Key,
+                            Total = dkGroup.Count(),
+                            Promedio_Antiguedad = (int)dkGroup.Average(x => DbFunctions.DiffDays(x.Employee.employeeHireDate, now))//(long)dkGroup.Average(x => DateTime.Now.Ticks-(x.Employee.employeeHireDate).Ticks)
+                        };
+            var results = query.ToList().Select(x => new GetKnowledgesSP_Result_Mapped
+            {
+                Conocimiento = x.Conocimiento,
+                Promedio_Antiguedad = x.Promedio_Antiguedad,
+                Total = x.Total
+            }).ToList();
+            return View(results);
         }
 
 
---
+++ FeLuisesScrumDEV/FeLuisesScrumDEV/viewModel/GetKnowledgesSP_Result_Mapped.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeLuisesScrumDEV.viewModel
+{
+    public partial class GetKnowledgesSP_Result_Mapped
+    {
+        public string Conocimiento { get; set; }
+        public Nullable<int> Total { get; set; }
+        public Nullable<int> Promedio_Antiguedad { get; set; }
+    }
+}
