23e144f39036ac5902cf673c87837d28e95ac2a9
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2020-06-09 22:10:22 -0600

    Controlador para el calendario y modelo para devolver los datos necesarios en el despliegue del calendario

---
+++ FeLuisesScrumDEV/FeLuisesScrumDEV/Controllers/CalendarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FeLuisesScrumDEV.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            // Obtengo datos por medio del procedimiento almacenado
+
+            // Mando los datos a la vista
+            return View();
+        }
+    }
+}
---
+++ FeLuisesScrumDEV/FeLuisesScrumDEV/Models/CalendarData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeLuisesScrumDEV.Controllers.Services
+{
+    public class CalendarData
+    {
+        public int idProjectFKPK { get; set; }
+        public int idModuleFKPK { get; set; }
+        public int idRequerimentPK { get; set; }
+        public short status { get; set; }
+        public System.DateTime startingDate { get; set; }
+        public System.DateTime endDate { get; set; }
+    }
+}
