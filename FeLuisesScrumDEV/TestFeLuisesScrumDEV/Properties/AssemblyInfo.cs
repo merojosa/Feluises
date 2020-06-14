9fc52bacc04c33e4e3320e24823e8248c699da28
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2020-06-13 21:13:07 -0600

    Proyecto de pruebas creado y configuración para conectarse a la base de datos

---
+++ FeLuisesScrumDEV/TestFeLuisesScrumDEV/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestFeLuisesScrumDEV")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestFeLuisesScrumDEV")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad492a8f-4fb5-4e9d-b579-8090f24ee3ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
